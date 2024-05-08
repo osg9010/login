4919fa11b66f2e3975f404e14cdbb2f1c2c12f37
osg9010 <osg3419010@naver.com>
2024-05-08 10:39:16 +0900

    로그인 서블릿이 제공하는 세션을 통한 로그인 구현

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -38,7 +39,7 @@ public class HomeController {
         model.addAttribute("member",loginMember);
         return "loginHome";
     }
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회회
@@ -47,6 +48,23 @@ public class HomeController {
         if(loginMember==null){
             return "home";
         }
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        // 로그인을 안한 처음 들어온 사람도 세션이 생길 수 있으니 false 로 생성
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return "home";
+        }
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if(loginMember==null){
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인성공
         model.addAttribute("member",loginMember);
         return "loginHome";
     }
