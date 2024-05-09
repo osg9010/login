c278791ccf651f41e8180219b78c0ed996291411
osg9010 <osg3419010@naver.com>
2024-05-09 10:04:18 +0900

    로그인 @SessionAttribute 를 통해 세션에 저장된 멤버를 가져와 로그인 처리 해당 애노테이션은 세션을 생성하지 않는다. server.servlet.session.tracking-modes=cookie 를 통해 처음 로그인시 URL에 세션아이디가 붙는걸 방지할수있다.

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -51,7 +52,7 @@ public class HomeController {
         model.addAttribute("member",loginMember);
         return "loginHome";
     }
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         // 로그인을 안한 처음 들어온 사람도 세션이 생길 수 있으니 false 로 생성
@@ -60,6 +61,18 @@ public class HomeController {
             return "home";
         }
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if(loginMember==null){
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인성공
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false) Member loginMember, Model model) {
+
+        // 로그인을 안한 처음 들어온 사람도 세션이 생길 수 있으니 false 로 생성
         if(loginMember==null){
             return "home";
         }
